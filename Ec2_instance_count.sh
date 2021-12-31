05533cac53aca977c1782f4c1a7be60bcbf6d910
msuri9849 <81887614+msuri9849@users.noreply.github.com>
2021-12-31 15:51:38 +0530

    Update Ec2_instance_count.sh

--- Ec2_instance_count.sh
+++ Ec2_instance_count.sh
@@ -4,19 +4,20 @@ pipeline {
     stages {
         stage('AWS Credentials') {
             steps {
-                withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1'){
+                withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1') {
                     sh 'echo "Hellow Jenkins"'
                 }
             }
         }
-        stage('Ec2_count'){
-            steps{
-                 withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1'){
-                sh '''for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d \'"\'); do
+        stage('Ec2_count') {
+            steps {
+                withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1') {
+                    sh '''Region_List=$(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d \'"\')
+                    for Region in $(Region_List); do
     #echo "Checking instance details in the region: $Region "
     #Instance_Region=$(sed -e \'s/-/_/g\' <<<"${Region}")
     #echo "${Instance_Region}"
-    Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\' | wc -l) >>out.log
+    Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\' | wc -l)
     if [ $Inscount -ne 0 ]; then
         echo "Checking instance details in the region: $Region "
         echo "================================================="
@@ -25,7 +26,7 @@ pipeline {
         aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\'
     fi
 done'''
-}
+                }
             }
         }
     }
