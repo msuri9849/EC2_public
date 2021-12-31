54ddd6748650f10bc9a1ade3b07061b17d5596b1
msuri9849 <81887614+msuri9849@users.noreply.github.com>
2021-12-31 18:52:02 +0530

    Update Ec2_instance_count.sh

--- Ec2_instance_count.sh
+++ Ec2_instance_count.sh
@@ -1,17 +1,27 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Ec2_count') {
+pipeline{
+    agent {
+        label any
+     }
+     environment {
+	GIT_TOKEN   = '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'
+	GIT_LOC     = 'msuri9849'
+	GIT_BRANCH = 'testing'
+     }
+     stage('Git_Repoclone'){
+	    steps{
+		sh '''
+		git clone -b ${GIT_BRANCH} https://${GIT_TOKEN}@github.com/${GIT_LOC}/EC2_public.git /tmp/EC2_public.git
+		'''
+	     }
+	 }
+	 stage('Ec2_count') {
             steps {
                 withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1') {
                 sh '''for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d \'"\'); do
-    #echo "Checking instance details in the region: $Region "
-    #Instance_Region=$(sed -e \'s/-/_/g\' <<<"${Region}")
-    #echo "${Instance_Region}"
-    Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\' | wc -l)
+        Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\' | wc -l)
     if [ $Inscount -ne 0 ]; then
-        echo "$Inscount" >Inscount_count_${Region}
+        echo "$Inscount" >/tmp/EC2_public/Inscount_count_${Region}
+		cd /tmp/EC2_public/
         git status
         git init
         git add .
@@ -22,11 +32,9 @@ pipeline {
         echo -e " Total Instance available in the region:$Region and count as: $Inscount\\n"
         aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\'
         echo " "
-
     fi
 done'''
                 }
             }
         }
-    }
-}
+		}
