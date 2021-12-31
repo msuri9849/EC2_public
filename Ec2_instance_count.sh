6e62f87695966894d9ae73941b78f09be2ed2215
msuri9849 <81887614+msuri9849@users.noreply.github.com>
2021-12-31 15:53:29 +0530

    Update Ec2_instance_count.sh

--- Ec2_instance_count.sh
+++ Ec2_instance_count.sh
@@ -12,8 +12,8 @@ pipeline {
         stage('Ec2_count') {
             steps {
                 withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1') {
-                    sh '''Region_List=$(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d \'"\')
-                    for Region in $(Region_List); do
+                sh '''
+                    for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d \'"\'); do
     #echo "Checking instance details in the region: $Region "
     #Instance_Region=$(sed -e \'s/-/_/g\' <<<"${Region}")
     #echo "${Instance_Region}"
