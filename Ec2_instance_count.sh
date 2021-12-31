46551ea04844e2782168587cf87d7a8c465333fb
msuri9849 <81887614+msuri9849@users.noreply.github.com>
2021-12-31 15:37:06 +0530

    Update Ec2_instance_count.sh

--- Ec2_instance_count.sh
+++ Ec2_instance_count.sh
@@ -1,14 +1,32 @@
-for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d '"'); do
+pipeline {
+    agent any
+
+    stages {
+        stage('AWS Credentials') {
+            steps {
+                withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1'){
+                    sh 'echo "Hellow Jenkins"'
+                }
+            }
+        }
+        stage('Ec2_count'){
+            steps{
+                 withAWS(credentials: '9cc1463e-6592-48a0-bc73-350382a29c98', region: 'ap-southeast-1'){
+                sh '''for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d \'"\'); do
     #echo "Checking instance details in the region: $Region "
-    #Instance_Region=$(sed -e 's/-/_/g' <<<"${Region}")
+    #Instance_Region=$(sed -e \'s/-/_/g\' <<<"${Region}")
     #echo "${Instance_Region}"
-    Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d '"' | wc -l)
+    Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\' | wc -l)
     if [ $Inscount -ne 0 ]; then
         echo "Checking instance details in the region: $Region "
         echo "================================================="
-        echo -e " Total Instance available in the region:$Region and count as: $Inscount\n"
+        echo -e " Total Instance available in the region:$Region and count as: $Inscount\\n"
         echo "$Inscount" >count_instance.log
-        cat count_instance.log
-        aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d '"'
+        aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\'
     fi
-done
+done'''
+}
+            }
+        }
+    }
+}
