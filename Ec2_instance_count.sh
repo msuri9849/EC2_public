e356dd26ce719bac53b0120bc20456ebe8b37d4e
msuri9849 <81887614+msuri9849@users.noreply.github.com>
2021-12-31 23:51:16 +0530

    Update Ec2_instance_count.sh

--- Ec2_instance_count.sh
+++ Ec2_instance_count.sh
@@ -1,8 +1,8 @@
-rm -rf EC2_public/
+sh '''rm -rf EC2_public/
 git clone -b testing git@github.com:msuri9849/EC2_public.git
 Count_ins=$(cat EC2_public.git/Inscount_count_ap-southeast-1)
-for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d '"'); do
-       Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d '"' | wc -l)
+for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d \'"\'); do
+       Inscount=$(aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\' | wc -l)
 	    if [[ $Inscount -ne 0 ]]; then
 		if [[ $Count_ins -eq  $Inscount ]];then
 		echo " There was no change in instance count"
@@ -14,9 +14,9 @@ for Region in $(aws ec2 describe-regions | jq ".Regions[].RegionName" | tr -d '"
 		cd ..
         echo "Checking instance details in the region: $Region "
         echo "================================================="
-        echo -e " Total Instance available in the region:$Region and count as: $Inscount\n"
-        aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d '"'
+        echo -e " Total Instance available in the region:$Region and count as: $Inscount\\n"
+        aws ec2 describe-instances --region "$Region" | jq ".Reservations[].Instances[].InstanceId" | tr -d \'"\'
         echo " "
 		fi
     fi
-done
+done'''
